8c7d61f90e6e90b0251511a443930610a38bd08d
ilkay Altınışık <ilkayalti@hotmail.com>
2025-04-18 21:10:39 +0300

    Update busineesService.ts

--- services/busineesService.ts
+++ services/busineesService.ts
@@ -21,7 +21,7 @@ export const fetchBusinesses = async (
 
     const response = await fetch(url.toString(), {
       headers: {
-        Authorization: `Bearer `, // API anahtarı
+        Authorization: `Bearer + ${process.env.API,}`, // API anahtarı
         Accept: "application/json",
       },
     });
